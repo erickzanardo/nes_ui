6de8ee4075bdca8c34a04b6afcb6482cbb1ef983
Elias Reis <eliasreis54@gmail.com>
2023-01-25 14:19:38 -0300

    chore(tests): set up alchemist

---
+++ test/flutter_test_config.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nes_ui/nes_ui.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  final enablePlatformTests =
+      !Platform.environment.containsKey('GITHUB_ACTIONS');
+  return AlchemistConfig.runWithConfig(
+    config: AlchemistConfig(
+      theme: flutterNesTheme(),
+      platformGoldensConfig:
+          AlchemistConfig.current().platformGoldensConfig.copyWith(
+                enabled: enablePlatformTests,
+              ),
+    ),
+    run: testMain,
+  );
+}
--- test/src/widgets/nes_container_test.dart
+++ test/src/widgets/nes_container_test.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nes_ui/nes_ui.dart';
@@ -16,5 +19,36 @@ void main() {
 
       expect(find.text('The Child'), findsOneWidget);
     });
+  });
+
+  group('NesContainer golden tests', () {
+    goldenTest(
+      'renders correctly',
+      builder: () => GoldenTestGroup(
+        scenarioConstraints: const BoxConstraints(maxWidth: 600),
+        children: [
+          GoldenTestScenario(
+            name: 'with child',
+            child: NesContainer(
+              child: Text('ListTile.title'),
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'with label',
+            child: NesContainer(
+              label: 'label',
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'with child and label',
+            child: NesContainer(
+              label: 'label',
+              child: Text('child'),
+            ),
+          ),
+        ],
+      ),
+      fileName: 'nes_container/nes_container_goldens',
+    );
   });
 }
