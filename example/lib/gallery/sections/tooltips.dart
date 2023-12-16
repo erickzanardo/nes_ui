45635fa850dd1a287806f93653ad9ceb50b5df05
maliksenpai <cramixzx@hotmail.com>
2023-12-16 13:57:40 +0300

    - Added tooltip for arrow direction

--- example/lib/gallery/sections/tooltips.dart
+++ example/lib/gallery/sections/tooltips.dart
@@ -19,21 +19,50 @@ class TooltipsSection extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             NesTooltip(
-              message: 'This is a tooltip',
+              message: 'This is a top tooltip',
               arrowPlacement: NesTooltipArrowPlacement.left,
               child: NesIcon(
                 iconData: NesIcons.exclamationMarkBlock,
               ),
             ),
             NesTooltip(
-              message: 'This is a tooltip',
+              message: 'This is a top tooltip',
               child: NesIcon(
                 iconData: NesIcons.exclamationMarkBlock,
               ),
             ),
             NesTooltip(
-              message: 'This is a tooltip',
+              message: 'This is a top tooltip',
               arrowPlacement: NesTooltipArrowPlacement.right,
+              child: NesIcon(
+                iconData: NesIcons.exclamationMarkBlock,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            NesTooltip(
+              message: 'This is a bottom tooltip',
+              arrowPlacement: NesTooltipArrowPlacement.left,
+              arrowDirection: NesTooltipArrowDirection.bottom,
+              child: NesIcon(
+                iconData: NesIcons.exclamationMarkBlock,
+              ),
+            ),
+            NesTooltip(
+              message: 'This is a bottom tooltip',
+              arrowDirection: NesTooltipArrowDirection.bottom,
+              child: NesIcon(
+                iconData: NesIcons.exclamationMarkBlock,
+              ),
+            ),
+            NesTooltip(
+              message: 'This is a bottom tooltip',
+              arrowPlacement: NesTooltipArrowPlacement.right,
+              arrowDirection: NesTooltipArrowDirection.bottom,
               child: NesIcon(
                 iconData: NesIcons.exclamationMarkBlock,
               ),
