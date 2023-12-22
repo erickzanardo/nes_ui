ab41ebfe52d99b53674c8e1268e3fe13639952e6
Erick Zanardo <erickzanardoo@gmail.com>
2023-12-22 11:24:43 -0300

    feat: adding widgetbook

---
+++ lib/widgetbook/widgetbook.directories.g.dart
@@ -0,0 +1,54 @@
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_import, prefer_relative_imports, directives_ordering
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AppGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:nes_ui/widgetbook/use_cases/buttons.dart' as _i2;
+import 'package:nes_ui/widgetbook/use_cases/checkboxes.dart' as _i3;
+import 'package:widgetbook/widgetbook.dart' as _i1;
+
+final directories = <_i1.WidgetbookNode>[
+  _i1.WidgetbookFolder(
+    name: 'widgets',
+    children: [
+      _i1.WidgetbookComponent(
+        name: 'NesButton',
+        useCases: [
+          _i1.WidgetbookUseCase(
+            name: 'error',
+            builder: _i2.error,
+          ),
+          _i1.WidgetbookUseCase(
+            name: 'normal',
+            builder: _i2.normal,
+          ),
+          _i1.WidgetbookUseCase(
+            name: 'primary',
+            builder: _i2.primary,
+          ),
+          _i1.WidgetbookUseCase(
+            name: 'success',
+            builder: _i2.success,
+          ),
+          _i1.WidgetbookUseCase(
+            name: 'warning',
+            builder: _i2.warning,
+          ),
+        ],
+      ),
+      _i1.WidgetbookLeafComponent(
+        name: 'NesCheckBox',
+        useCase: _i1.WidgetbookUseCase(
+          name: 'default',
+          builder: _i3.checkbox,
+        ),
+      ),
+    ],
+  )
+];
---
+++ lib/widgetbook/widgets/widgets.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+
+class UseCaseDecorator extends StatelessWidget {
+  const UseCaseDecorator({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.grey[600],
+      ),
+      child: SizedBox.expand(
+        child: child,
+      ),
+    );
+  }
+}
