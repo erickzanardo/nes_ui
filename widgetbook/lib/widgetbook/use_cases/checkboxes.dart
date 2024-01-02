b1ce08ea9c71fae54ef8112b230deecfc1ca7a5c
Erick Zanardo <erickzanardoo@gmail.com>
2024-01-02 12:05:58 -0300

    format

--- widgetbook/lib/widgetbook/use_cases/checkboxes.dart
+++ widgetbook/lib/widgetbook/use_cases/checkboxes.dart
@@ -6,20 +6,20 @@ import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 Widget build() {
   var selected = false;
-    return Center(
-      child: StatefulBuilder(
-        builder: (context, setState) {
-          return NesCheckBox(
-            onChange: (value) {
-              setState(() {
-                selected = value;
-              });
-            },
-            value: selected,
-          );
-        },
-      ),
-    );
+  return Center(
+    child: StatefulBuilder(
+      builder: (context, setState) {
+        return NesCheckBox(
+          onChange: (value) {
+            setState(() {
+              selected = value;
+            });
+          },
+          value: selected,
+        );
+      },
+    ),
+  );
 }
 
 @widgetbook.UseCase(
