930bd3162ac100e3527402de1852e769d6586aba
Erick Zanardo <erickzanardoo@gmail.com>
2024-03-07 15:22:09 -0300

    feat: open sprites on mini sprite

--- lib/src/widgets/nes_icon16.dart
+++ lib/src/widgets/nes_icon16.dart
@@ -1,8 +1,16 @@
 // ignore_for_file: lines_longer_than_80_chars
+import 'package:mini_sprite/mini_sprite.dart';
 import 'package:nes_ui/nes_ui.dart';
 
 /// Built in library of 16x16 icons for the Flutter Nes Design library.
 /// Use [NesIcons] to access the icons.
 class NesIcons16 {
   NesIcons16._();
+
+  /// A check icon.
+  static final check = NesIconData(
+    MiniSprite.fromDataString(
+      '8,8;6,-1;2,0;5,-1;1,0;1,1;1,0;4,-1;1,0;2,1;3,0;1,-1;1,0;2,1;1,0;1,-1;1,0;1,1;1,0;2,1;1,0;2,-1;1,0;3,1;1,0;4,-1;1,0;1,1;1,0;6,-1;1,0;5,-1',
+    ),
+  );
 }
