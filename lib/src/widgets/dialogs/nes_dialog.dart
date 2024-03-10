69129c91347d5ab56454137adc8c6646e0534f17
Erick <erickzanardoo@gmail.com>
2024-03-10 11:26:55 -0300

    feat: Close dialogs on esc

--- lib/src/widgets/dialogs/nes_dialog.dart
+++ lib/src/widgets/dialogs/nes_dialog.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nes_ui/nes_ui.dart';
+
+class _CloseDialogIntent extends Intent {
+  const _CloseDialogIntent();
+}
 
 /// {@template nes_confirm_dialog}
 /// A dialog that shows a child.
@@ -63,38 +68,56 @@ class NesDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      child: Material(
-        color: Colors.transparent,
-        child: IntrinsicWidth(
-          stepHeight: 0.56,
-          child: SizedBox.expand(
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                NesContainer(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Center(
-                      child: child,
-                    ),
+    return Shortcuts(
+      shortcuts: <LogicalKeySet, Intent>{
+        LogicalKeySet(LogicalKeyboardKey.escape): const _CloseDialogIntent(),
+      },
+      child: Actions(
+        actions: <Type, Action<Intent>>{
+          _CloseDialogIntent: CallbackAction<_CloseDialogIntent>(
+            onInvoke: (intent) {
+              Navigator.of(context).pop();
+              return null;
+            },
+          ),
+        },
+        child: Focus(
+          autofocus: true,
+          child: Align(
+            child: Material(
+              color: Colors.transparent,
+              child: IntrinsicWidth(
+                stepHeight: 0.56,
+                child: SizedBox.expand(
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      NesContainer(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Center(
+                            child: child,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: -8,
+                        top: -8,
+                        child: NesButton(
+                          type: NesButtonType.error,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: NesIcon(
+                            size: const Size(16, 16),
+                            iconData: NesIcons.close,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Positioned(
-                  right: -8,
-                  top: -8,
-                  child: NesButton(
-                    type: NesButtonType.error,
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: NesIcon(
-                      size: const Size(16, 16),
-                      iconData: NesIcons.close,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
