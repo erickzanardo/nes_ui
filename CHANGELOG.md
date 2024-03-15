26669506ae81053fdd78ba1e901980ef85a6591e
Erick <erickzanardoo@gmail.com>
2024-03-15 11:31:53 -0300

    changelog (#137)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 0.20.0
+ - feat: adding different frames to `NesDialog`s
+
 # 0.19.0
  - feat: adding `NesSectionHeader`
  - feat: adding `NesIcons16.check`
@@ -9,7 +12,6 @@
  - feat: adding `NesIcons16.rightArrowIndicator`
  - feat: adding `NesIcons16.topArrowIndicator`
  - feat: adding `NesIcons16.bottomArrowIndicator`
- - feat: adding frame customization to `NesDialog`
 
 # 0.18.0
  - feat: add `NesIcons.wrench`
--- example/lib/gallery/sections/dialogs.dart
+++ example/lib/gallery/sections/dialogs.dart
@@ -15,7 +15,7 @@ class DialogsSection extends StatelessWidget {
           style: theme.textTheme.displayMedium,
         ),
         const SizedBox(height: 16),
-        Row(
+        Wrap(
           children: [
             NesButton(
               type: NesButtonType.normal,
@@ -54,6 +54,89 @@ class DialogsSection extends StatelessWidget {
                   message: 'What is the coolest NES game?',
                 );
                 showSnack(inputData);
+              },
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        Text(
+          'Dialogs with NesWindowDialogFrame',
+          style: theme.textTheme.displayMedium,
+        ),
+        const SizedBox(height: 16),
+        Wrap(
+          children: [
+            NesButton(
+              type: NesButtonType.normal,
+              child: const Text('Plain'),
+              onPressed: () {
+                NesDialog.show<void>(
+                  context: context,
+                  builder: (_) => const Text('Hello World'),
+                  frame: const NesWindowDialogFrame(),
+                );
+              },
+            ),
+            const SizedBox(width: 16),
+            NesButton(
+              type: NesButtonType.normal,
+              child: const Text('Confirmation'),
+              onPressed: () {
+                NesConfirmDialog.show(
+                  context: context,
+                  frame: const NesWindowDialogFrame(),
+                );
+              },
+            ),
+            const SizedBox(width: 16),
+            NesButton(
+              type: NesButtonType.normal,
+              child: const Text('Input'),
+              onPressed: () async {
+                void showSnack(String? inputData) {
+                  if (inputData != null) {
+                    NesSnackbar.show(
+                      context,
+                      text: inputData,
+                    );
+                  }
+                }
+
+                final inputData = await NesInputDialog.show(
+                  context: context,
+                  message: 'What is the coolest NES game?',
+                  frame: const NesWindowDialogFrame(),
+                );
+                showSnack(inputData);
+              },
+            ),
+            const SizedBox(width: 16),
+            NesButton(
+              type: NesButtonType.normal,
+              child: const Text('Plain with icon'),
+              onPressed: () {
+                NesDialog.show<void>(
+                  context: context,
+                  builder: (_) => const Text('Hello World'),
+                  frame: NesWindowDialogFrame(
+                    leftIcon: NesIcons.bell,
+                  ),
+                );
+              },
+            ),
+            const SizedBox(width: 16),
+            NesButton(
+              type: NesButtonType.normal,
+              child: const Text('Plain with icon and title'),
+              onPressed: () {
+                NesDialog.show<void>(
+                  context: context,
+                  builder: (_) => const Text('Hello World'),
+                  frame: NesWindowDialogFrame(
+                    leftIcon: NesIcons.bell,
+                    title: 'Hello',
+                  ),
+                );
               },
             ),
           ],
