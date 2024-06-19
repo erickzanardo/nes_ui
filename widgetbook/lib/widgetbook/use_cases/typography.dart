43ee4a2ebd94da4a0a821e4de63f4cff5750939c
Erick Zanardo <erickzanardoo@gmail.com>
2024-06-19 10:20:03 -0300

    lint

--- widgetbook/lib/widgetbook/use_cases/typography.dart
+++ widgetbook/lib/widgetbook/use_cases/typography.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs
 
 import 'package:flutter/material.dart';
-import 'package:nes_ui/nes_ui.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 Widget build({
