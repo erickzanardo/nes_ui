fa131f4e34a9fe6b3897cadffd464de4e86d1c5f
Erick Zanardo <erickzanardoo@gmail.com>
2025-11-18 14:22:58 -0300

    drunken import

--- lib/src/widgets/nes_button.dart
+++ lib/src/widgets/nes_button.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:nes_ui/nes_ui.dart';
 import 'package:nes_ui/src/extensions/color.dart';
